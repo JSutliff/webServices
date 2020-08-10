39d4ce4118855ad6fe0331f1d7307e672ffb6918
JSutliff <sutliff.jason@gmail.com>
2020-08-10 15:02:21 -0700

    big update

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -23,6 +23,22 @@ var router = require("express").Router();
 //   }
 // });
 
+router.get("/domain-with-ssl", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/domain-with-ssl.html"));
+});
+
+router.get("/web-design", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/website-design.html"));
+});
+
+router.get("/business-website-solutions", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/websites.html"));
+});
+
+router.get("/local-seo", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/local-seo.html"));
+});
+
 router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
